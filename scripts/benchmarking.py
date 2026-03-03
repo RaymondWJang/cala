237f418210adaf311f52331811608883c3e4c40e
Raymond <raymond.w.jang@gmail.com>
2026-03-02 16:38:26 -0800

    docs: docs for benchmarkers

--- scripts/benchmarking.py
+++ scripts/benchmarking.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from pathlib import Path
 
 import cv2
 import matplotlib
@@ -8,7 +9,15 @@ from matplotlib import pyplot as plt
 from noob import SynchronousRunner, Tube
 
 from cala.nodes.io import stream
-from cala.nodes.prep import Anchor, blur, butter, package_frame, remove_mean
+from cala.nodes.prep import (
+    Anchor,
+    blur,
+    butter,
+    package_frame,
+    remove_mean,
+    GlowRemover,
+)
+from cala.nodes.prep.motion import _prepare
 from cala.testing.util import total_gradient_magnitude
 
 sns.set_style("whitegrid")
@@ -18,21 +27,26 @@ matplotlib.rc("font", **font)
 
 
 VIDEOS = [
-    "minian/msCam1.avi",
-    "minian/msCam2.avi",
-    "minian/msCam3.avi",
-    "minian/msCam4.avi",
-    "minian/msCam5.avi",
-    "minian/msCam6.avi",
-    "minian/msCam7.avi",
-    "minian/msCam8.avi",
-    "minian/msCam9.avi",
-    "minian/msCam10.avi",
-    # "long_recording/0.avi",
-    # "long_recording/1.avi",
-    # "long_recording/2.avi",
-    # "long_recording/3.avi",
-    # "long_recording/4.avi",
+    # "minian/msCam1.avi",
+    # "minian/msCam2.avi",
+    # "minian/msCam3.avi",
+    # "minian/msCam4.avi",
+    # "minian/msCam5.avi",
+    # "minian/msCam6.avi",
+    # "minian/msCam7.avi",
+    # "minian/msCam8.avi",
+    # "minian/msCam9.avi",
+    # "minian/msCam10.avi",
+    "long_recording/0.avi",
+    "long_recording/1.avi",
+    "long_recording/2.avi",
+    "long_recording/3.avi",
+    "long_recording/4.avi",
+    "long_recording/5.avi",
+    "long_recording/6.avi",
+    "long_recording/7.avi",
+    "long_recording/8.avi",
+    "long_recording/9.avi",
 ]
 
 
@@ -43,7 +57,7 @@ def preprocess(arr, idx):
     return remove_mean(frame, orient="both")
 
 
-def test_encode():
+def test_write_raw_movie():
     gen = stream(VIDEOS)
 
     fourcc = cv2.VideoWriter_fourcc(*"FFV1")
@@ -56,15 +70,15 @@ def test_encode():
     out.release()
 
 
-def test_motion_movie():
+def test_write_stable_movie():
     """
-    For testing how well the motion correction performs with real movie
-
+    For testing how well motion correction performs with real movie
+    See for yourself
     """
     gen = stream(VIDEOS)
 
     fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-    out = cv2.VideoWriter("motion_test.avi", fourcc, 60.0, (600, 1200))
+    out = cv2.VideoWriter("motion_test.avi", fourcc, 60.0, (752, 960))
 
     stab = Anchor()
 
@@ -79,11 +93,43 @@ def test_motion_movie():
     out.release()
 
 
+def test_write_signal_movie():
+    """
+    For testing how well the entire preprocessing line performs with real movie
+    See for yourself
+    """
+    gen = stream(VIDEOS)
+
+    fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+    out = cv2.VideoWriter("signal_test.avi", fourcc, 60.0, (752, 960))
+
+    stab = Anchor()
+    bgrm = GlowRemover()
+
+    for idx, arr in enumerate(gen):
+        frame = preprocess(arr, idx)
+        matched = stab.stabilize(frame)
+        signal = bgrm.process(matched)
+        combined = np.concat([matched.array.values, signal.array.values], axis=0)
+
+        frame_bgr = cv2.cvtColor(combined.astype(np.uint8), cv2.COLOR_GRAY2BGR)
+        out.write(frame_bgr)
+
+    out.release()
+
+
 def test_motion_crisp_pics():
     """
-    For generating a mean summary frame across raw vs. motion-corrected video.
-    The motion-corrected video should have a much crisper summary picture.
+    quantify how crisp is a summary image before and after registration.
+    This can be done by computing the norm of the gradient field of the image at all pixels.
+    Intuitively, a dataset with non-registered motion will have a blurred mean image,
+    resulting in a lower value for the total gradient field norm.
 
+    Currently not working properly because of the remaining optical artifacts. (vertical bands)
+    These artifacts move with camera, which becomes clearer if the image is NOT motion-corrected.
+
+    References:
+        [1] NormCorre, https://www.sciencedirect.com/science/article/pii/S0165027017302753
     """
 
     gen = stream(VIDEOS)
@@ -115,8 +161,23 @@ def test_motion_crisp_pics():
 
 def test_motion_mean_corr():
     """
-    For testing how well the motion correction performs with real movie
+    To evaluate the results of the motion correction algorithm across the different frames,
+    we use a metric that is based on the similarity (pixel-wise, Pearson's correlation coefficient r)
+    between the mean image across time and each individual frame.
+    Intuitively, an increase in the correlation coefficient for a given frame
+    indicates a better alignment with the mean.
+
+    Similar metrics have been used before for assessing the quality of registration algorithms
+    in the context of fluorescence microscopy (Lee et al., 2014).
+    To account for border effects during registration,
+    a number of pixels around each boundary (e.g., equal to the maximum shift in each direction over time)
+    is removed prior to computing the correlation coefficients.
+
+    References:
+        [1] NormCorre, https://www.sciencedirect.com/science/article/pii/S0165027017302753
     """
+    figure_dir = Path("figures/motion")
+
     gen = stream(VIDEOS)
 
     stab = Anchor()
@@ -126,6 +187,15 @@ def test_motion_mean_corr():
     for idx, arr in enumerate(gen):
         frame = preprocess(arr, idx)
         matched = stab.stabilize(frame)
+        if idx == 500:
+            curr = _prepare(frame.array, stab.dog_kwargs, stab.gauss_kwargs).values
+            cv2.imwrite(
+                figure_dir / "anchors.png",
+                np.concatenate([curr, stab._local.values, stab._global.values], axis=1),
+            )
+            cv2.imwrite(figure_dir / "curr.png", curr)
+            cv2.imwrite(figure_dir / "local_anchor.png", stab._local.values)
+            cv2.imwrite(figure_dir / "global_anchor.png", stab._global.values)
         raws.append(frame.array.values)
         stabs.append(matched.array.values)
 
@@ -138,20 +208,24 @@ def test_motion_mean_corr():
     raw_cms = [np.corrcoef(r[20:-20, 20:-20].flatten(), raw_mean.flatten())[0, 1] for r in raws]
     stab_cms = [np.corrcoef(s[20:-20, 20:-20].flatten(), stab_mean.flatten())[0, 1] for s in stabs]
 
-    fig, ax = plt.subplots(figsize=(24, 10))
+    fig, ax = plt.subplots(figsize=(12, 5))
     plt.plot(raw_cms)
     plt.plot(stab_cms)
     plt.legend(["raw", "stabilized"], loc="upper right")
-    plt.title("Mean Correlation")
-    plt.xlabel("frame")
-    plt.ylabel("correlation")
+    plt.xlabel("Frame Index")
+    plt.ylabel("Correlation")
     plt.tight_layout()
-    plt.savefig("mc.png")
+
+    figure_dir.mkdir(parents=True, exist_ok=True)
+    plt.savefig(figure_dir / "mean_corr.png")
 
     assert False
 
 
-def test_with_movie():
+def test_whole_pipeline():
+    """
+    Run the entire pipeline with Minian data
+    """
     tube = Tube.from_specification("with-minian")
     runner = SynchronousRunner(tube=tube)
     processed_vid = runner.run()
@@ -164,7 +238,11 @@ def test_with_movie():
     out.release()
 
 
-def test_processing_speed():
+def test_speed_per_frame():
+    """
+    Test how long it takes for the entire cala to process each frame.
+    Export a plot showing time taken.
+    """
     tube = Tube.from_specification("with-minian")
     runner = SynchronousRunner(tube=tube)
     gen = runner.iter()
@@ -188,8 +266,11 @@ def test_processing_speed():
     plt.savefig("frame_speed.png")
 
 
-def test_deglow():
-
+def test_deglow_write_image():
+    """
+    Write a deglowed frame by accumulating the frames that are preprocessed until motion correction,
+    grab the last frame and subtract the global minimum pixel value through time.
+    """
     gen = stream(VIDEOS[:3])
 
     stab = Anchor()
@@ -202,3 +283,60 @@ def test_deglow():
 
     deglowed = stabs[-1] - np.min(stabs, axis=0)
     plt.imsave("deglowed.png", deglowed, cmap="gray")
+
+
+def test_preprocessing_write_image():
+    """
+    A temporary function for hotswapping preprocessing steps
+    and plot results / diffs fast.
+    """
+    images = {}
+    gen = stream(VIDEOS)
+
+    stab = Anchor()
+    eraser = GlowRemover()
+    raws = []
+    stabs = []
+
+    for idx, arr in enumerate(gen):
+        frame = preprocess(arr, idx)
+        matched = stab.stabilize(frame)
+        signal = eraser.process(matched)
+        if idx == 500:
+            break
+    arr = next(gen)
+    frame = preprocess(arr, idx)
+    matched = stab.stabilize(frame)
+    signal = eraser.process(matched)
+
+    frame = package_frame(arr, 0)
+    hot_pixel = blur(frame, method="median", kwargs={"ksize": 3})
+    buttered = butter(hot_pixel, {})
+    delined = remove_mean(buttered, "both")
+    images["pre"] = buttered.array.values
+    images["post"] = delined.array.values
+    images["diff"] = buttered.array.values - delined.array.values
+    # crop = slice(None, int(arr.shape[0] / 3)), slice(int(arr.shape[1] * 3 / 4), None)
+    # for k, v in images.items():
+    #     images[k] = circle_points(
+    #         np.clip(v, 0, None).astype(np.uint8),
+    #         zip(*np.where(diff > 50)[::-1]),
+    #         radius=20,
+    #         color=(0, 0, 255),
+    #         thickness=2,
+    #     )[crop]
+    images["diff"] = images["diff"] / images["diff"].max() * 255
+    all = np.concatenate([image for image in images.values()], axis=1)
+    figure_dir = Path("figures/deline")
+    figure_dir.mkdir(parents=True, exist_ok=True)
+    cv2.imwrite(figure_dir / "all.png", all)
+    for k in images:
+        cv2.imwrite(figure_dir / f"{k}.png", images[k])
+
+
+def test_recursive():
+    gen = stream(VIDEOS[:3])
+    tube = Tube.from_specification("cala-demo", {"cell_size": 8})
+    runner = SynchronousRunner(tube)
+    for idx, arr in enumerate(gen):
+        res = runner.process(frame=arr, index=idx)
